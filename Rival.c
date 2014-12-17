90c9b78313cb41234b9829e1c06220181245ce2f
Sam Caulfield <sam@samcaulfield.com>
2014-12-17 11:14:03 +0000

    A combat function has been added.
    The two combatants take a hit at one another. It doesn't matter who goes first.

--- Rival.c
+++ Rival.c
@@ -120,6 +120,16 @@ void drawHelpText(struct Player me, bool myTurn, int movesLeft)
     : "It's your opponent's turn!", movesLeft);
 }
 
+void attack(struct Player *a, struct Player *b)
+{
+  int aStrength = a->attack - b->defence;
+  aStrength = (aStrength < 0) ? 0 : aStrength;
+  b->health -= aStrength;
+  int bStrength = b->attack - a->defence;
+  bStrength = (bStrength < 0) ? 0 : bStrength;
+  a->health -= bStrength;
+}
+
 int distance(struct Player a, struct Player b)
 {
   return abs(a.x - b.x) + abs(a.y - b.y);
